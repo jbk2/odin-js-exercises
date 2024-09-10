b1be52a9fdfb649b3b33306baa1efe70910d3062
jbk2 <james@humanbenefits.com>
2024-09-10 16:39:21 +0200

    Completed exercise_3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -4,7 +4,7 @@ const repeatString = function(string, num) {
     return 'ERROR';
   }
 
-  for (i = 1; i <= num; i++) {
+  for (let i = 1; i <= num; i++) {
     output += string;
   }
   return output;
