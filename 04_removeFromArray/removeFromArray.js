d644393f542c509dc661b619fb442ca2fb9f733a
jbk2 <james@humanbenefits.com>
2024-09-10 21:34:43 +0200

    Completed exercise_4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
 const removeFromArray = function() {
+  let noOfElementsToCheck = arguments.length - 1;
+  let array = arguments[0];
+  for(let i = 1; i <= noOfElementsToCheck; i++) {
+    let valueToCheck = arguments[i]
+    array = array.filter((element) => element !== valueToCheck)
+  }
+  return array;
 };
 
 // Do not edit below this line
