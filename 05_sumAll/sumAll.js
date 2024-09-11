dbfdb13b2c520056bfbfecbf59a15b10a27e4e3d
jbk2 <james@humanbenefits.com>
2024-09-11 17:04:16 +0200

    Complete exercise_5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+  if((!Number.isInteger(start) || !Number.isInteger(end)) || ( start < 0 || end <0 )) {
+    return 'ERROR';
+  };
+
+  const smaller = Math.min(start, end)
+  const larger = Math.max(start, end)
+  
+  let sum = 0;
+  
+  for(i = smaller; i <= larger; i++){
+    sum = sum += i 
+  };
+
+  return sum;
 };
 
 // Do not edit below this line
