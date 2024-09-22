48e9fe616b6e7814602056a3b60be21141126e33
jbk2 <james@humanbenefits.com>
2024-09-22 16:47:05 +0200

    Passing 2 of ex07's tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,8 @@
-const convertToCelsius = function() {
+//  -32 * (5/9)
+const convertToCelsius = function(fahrenheitValue) {
+  let exactCelcius = (fahrenheitValue - 32) * (5 / 9) 
+  let roundedCelciusString = exactCelcius.toFixed(1);
+  return parseFloat(roundedCelciusString);
 };
 
 const convertToFahrenheit = function() {
