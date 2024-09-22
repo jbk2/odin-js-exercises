0a6e99d740168df1a8b6f51be9d5386d1535bf21
jbk2 <james@humanbenefits.com>
2024-09-22 16:23:05 +0200

    Complete excercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  if(divisibleByFourHundered(year)) {
+    return true;
+  } else if (divisibleByFour(year) && !divisibleByOneHundered(year)) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
+function divisibleByFour(n) {
+  return n % 4 === 0;
+};
+
+function divisibleByOneHundered(n) {
+  return n % 100 === 0;
+};
+
+function divisibleByFourHundered(n) {
+  return n % 400 === 0;
 };
 
 // Do not edit below this line
