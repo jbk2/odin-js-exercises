aa10d7ef29e54fb6ffa9e2ba0b8cf09605268afb
jbk2 <james@humanbenefits.com>
2024-09-23 16:03:18 +0200

    Complete Ex09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let lowercase = string.toLowerCase();
+  let strippedString = lowercase.replace(/[^a-z0-9]/gi, '');
+  let reversedString = strippedString.split('').reverse().join('');
+  return strippedString == reversedString ? true : false;
 };
 
 // Do not edit below this line
