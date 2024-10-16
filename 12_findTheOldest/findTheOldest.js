19a113aa59df50a28d992afca766669d7d53ad74
jbk2 <james@humanbenefits.com>
2024-10-16 11:34:45 +0200

    Complete ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const getAge = (birth, death) => {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
+
+const findTheOldest = function(people) {
+  return people.reduce((oldest, currentPerson) => {
+    let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
